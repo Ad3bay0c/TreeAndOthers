99dafdfdb10c40a0738cc16cd6994686f2070fe0
tambarie <aclintonadebayo@gmail.com>
2021-09-08 14:32:58 +0100

    Main File

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	//var n int
+	//fmt.Scanf("%d", &n)
+	//fmt.Printf("N value is .....%d\n", n)
+	//
+	//for i := 0; i < 3; i++ {
+	//	var val, left, right int
+	//	fmt.Scanf("%d %d %d", &val, &left, &right)
+	//	fmt.Printf("Val %d, Left %d, Right %d\n", val, left, right)
+	//}
+
+	//t := &tree.Tree{Value: 25}
+	//t.AddToTree(27)
+	//t.AddToTree(26)
+	//t.AddToTree(1)
+	//t.AddToTree(13)
+	//t.AddToTree(30)
+	//t.PrintInorder()
+	//fmt.Println()
+	//t.PrintPreOrder()
+	//fmt.Println()
+	//t.PrintPostOrder()
+	//fmt.Println()
+	//
+	//t.DeleteFromTree(1)
+	//t.PrintInorder()
+	//fmt.Println()
+	var result string
+	 result, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Println(strings.Split(strings.TrimRight(result, "\n"), " "))
+}
